06267ead9fc7483fbb05bfd29431e08c582dd4dc
yereverluvinunclebert <dean.beedell@lightquick.co.uk>
2012-11-14 01:35:34 +0000

    deprecated ereg occurrences replaced by preg_match
    now compatible with php 5.3

--- modules/mod_banners.php
+++ modules/mod_banners.php
@@ -81,13 +81,12 @@ if ($numrows){
 
 		if (trim( $banner->custombannercode )) {
 			echo $banner->custombannercode;
-		} else if (eregi( "(\.bmp|\.gif|\.jpg|\.jpeg|\.png)$", $banner->imageurl )) {
-			$imageurl 	= $mosConfig_live_site .'/images/banners/'. $banner->imageurl;
+		} else if(preg_match("/(\.bmp|\.gif|\.jpg|\.jpeg|\.png)$/i", $banner->image_url)) {
+                        $imageurl 	= $mosConfig_live_site .'/images/banners/'. $banner->imageurl;
 			$link		= sefRelToAbs( 'index.php?option=com_banners&amp;task=click&amp;bid='. $banner->bid );
 			if( !defined('_BANNER_ALT') ) DEFINE('_BANNER_ALT','Advertisement');
 			echo '<a href="'. $link .'" target="_blank"><img src="'. $imageurl .'" border="0" alt="Advertisement" /></a>';
-
-		} else if (eregi("\.swf$", $banner->imageurl)) {
+		} elseif(preg_match("/.swf/", $banner->image_url)) {
 			$imageurl 	= "$mosConfig_live_site/images/banners/".$banner->imageurl;
 			echo "<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0\" border=\"5\">
 					<param name=\"movie\" value=\"$imageurl\"><embed src=\"$imageurl\" loop=\"false\" pluginspage=\"http://www.macromedia.com/go/get/flashplayer\" type=\"application/x-shockwave-flash\"></embed></object>";
